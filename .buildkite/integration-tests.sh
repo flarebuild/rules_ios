55242082395f2ea5b1909d74f22fe00178c893be
Zach Gray <zach@flare.build>
2022-05-11 14:17:41 +0200

    adding buildkite

---
+++ .buildkite/integration-tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash --login --noprofile
+
+source "$(git rev-parse --show-toplevel)"/.buildkite/common.sh
+source "$(git rev-parse --show-toplevel)"/.github/xcode_select.sh
+
+configure_user_bazelrc
+
+# clean
+bazel clean
+
+# builds and tests with bazel
+bazel_with_flareparse test --local_test_jobs=1 -- //... -//tests/ios/...
+bazel_with_flareparse test --local_test_jobs=1 --apple_platform_type=ios --deleted_packages='' -- //tests/ios/...
